04ca6e825efa28580752fc91343093a8f48a5add
eric-vinn <eric.vincent@ti.ukdw.ac.id>
2023-01-03 12:02:52 +0700

    first commit

---
+++ soal1.py
@@ -0,0 +1,18 @@
+print("****** Kredit Keaktifan Mahasiswa ******")
+print("(Student Activities Credit")
+print("1. Menambahka Kegiatan")
+print("2. Menampilkan Jumlah Poin")
+print("3. Keluar")
+print("---------------------")
+pilihan = input("Silahkan masukan pilihan Anda: ")
+nama = input("Nama Kegiatan: ")
+tanggal = input("Tanggal Kegiatan: ")
+print("Pilihan Kategori Kegiatan: ")
+print("- Prestasi")
+print("- Organisasi")
+print("- Kepanitiaan")
+print("- Rekognisi")
+kegiatan = input("Masukkan Kategori kegiatan: ")
+
+if kegiatan
+
